b764d276a565699d0199173e5b10bad283c5a584
joaquinoliba <joaquinoliba26@gmail.com>
2023-02-09 17:15:47 -0300

    muestra el titulo y la fecha

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const db = require("./db")
+const db = require("./db");
 
-const { Sequelize, Model, DataTypes } = require("sequelize")
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 /* 
 const mainController = require("./controllers/mainController")
@@ -10,32 +10,36 @@ const articleController = require("./controllers/articleController") */
 
 // Pagina Home
 router.get("/", async (req, res) => {
-    const [articulos] = await db("SELECT * FROM articles")
-    res.render("home", { articulos })
-})
+  const [articulos] = await db(
+    "SELECT * FROM articles ORDER BY create_at DESC"
+  );
+  res.render("home", { articulos });
+});
 
 // Pagina editar Articulo
 router.get("/articles/edit/:id", async (req, res) => {
-    const [article] = await db(`SELECT * FROM articles WHERE id = ${req.params.id} `)
-    console.log(article);
-    res.render("articleEdit", { article: article[0] })
-})
+  const [article] = await db(
+    `SELECT * FROM articles WHERE id = ${req.params.id} `
+  );
+  console.log(article);
+  res.render("articleEdit", { article: article[0] });
+});
 
 // Pagina agregar Articulo
 router.get("/articles/add/", async (req, res) => {
-    res.render("articleAdd")
-})
+  res.render("articleAdd");
+});
 
 // Pagina Articulo
 router.get("/articles", (req, res) => {
-    res.render("article")
-})
+  res.render("article");
+});
 
 // Pagina panel Administrador
 router.get("/admin", async (req, res) => {
-    const [articles] = await db("SELECT * FROM articles")
-    const [authors] = await db("SELECT * FROM authors")
-    res.render("panel-admin", { articles, authors })
-})
+  const [articles] = await db("SELECT * FROM articles");
+  const [authors] = await db("SELECT * FROM authors");
+  res.render("panel-admin", { articles, authors });
+});
 
-module.exports = router
+module.exports = router;
