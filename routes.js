b0e7d847fb948492e15ad0719469c17681e9b3eb
magguer <martintelaguer@gmail.com>
2023-02-09 12:27:44 -0300

    controllers_routes

---
+++ routes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const mainController = require("./controllers/mainController")
+const articleController = require("./controllers/usuariosControllers")
+const userController = require("./controllers/usuariosControllers")
