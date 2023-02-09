362546f6b9104a9000669a04c34eb696817571fc
magguer <martintelaguer@gmail.com>
2023-02-09 16:38:19 -0300

    addArticle_editArticle_added

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const methodOverride = require('method-override')
+
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const port = 3000
 
 app.set("view engine", "ejs");
+
+app.use(methodOverride('_method'))
+
+// - URLEncoded - Permite recibir la información enviada mediate el metodo POST, (sustituimos req.query por req.body)
+app.use(express.urlencoded({ extended: true }))
+
 
 //Activación de Rutas
 app.use(routes);
