2511bc4eb67f2316b5c68a90081f5d13aa7c024b
ManuelIllada <ignacio.illada@fing.edu.uy>
2023-02-10 15:50:36 -0300

    article anda sin comments

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -4,54 +4,55 @@ const { es } = require("date-fns/locale");
 
 //Página Todos los Articulos
 const index = async (req, res) => {
-    const [articulos] = await db("SELECT * FROM articles");
-    res.render("home", { articulos, format, es });
+  const [articulos] = await db("SELECT * FROM articles");
+  res.render("home", { articulos, format, es });
 };
 
 //Página Crear Articulos
 const addArticlePage = (req, res) => {
-    res.render("articleAdd");
+  res.render("articleAdd");
 };
 
 //Página editar un Articulo
 const editArticlePage = async (req, res) => {
-    const [article] = await db(
-        `SELECT * FROM articles WHERE id = ${req.params.id} `
-    );
-    console.log(article);
-    res.render("articleEdit", { article: article[0] });
+  const [article] = await db(
+    `SELECT * FROM articles WHERE id = ${req.params.id} `
+  );
+  console.log(article);
+  res.render("articleEdit", { article: article[0] });
 };
 
 //Página datos de un Articulo
 const articlePage = async (req, res) => {
-    const [article] = await db(
-        `SELECT * FROM articles WHERE id = ${req.params.id} `
-    );
-    console.log(article);
-    res.render("article", { article: article[0], format, es });
+  const [article] = await db(
+    `SELECT * FROM articles WHERE id = ${req.params.id} `
+  );
+  console.log(article);
+  res.render("article", { article: article[0], format, es });
 };
 
 //Página admin de Articulos
 const admArticulosPAge = async (req, res) => {
-    const [articles] = await db("SELECT * FROM articles");
-    res.render("panel-admin", { articles, format, es });
-}
+  const [articles] = await db("SELECT * FROM articles");
+  res.render("panel-admin", { articles, format, es });
+};
 
 //Insertar un Articulo
 const addArticleFunction = "";
 
 //Editar un Articulo
-const editArticleFunction = ""
+const editArticleFunction = "";
 
 //Eliminar un Articulo
 const deleteArticle = "";
 
 module.exports = {
-    index,
-    addArticlePage,
-    addArticleFunction,
-    editArticlePage,
-    editArticleFunction,
-    deleteArticle,
-    admArticulosPAge,
+  index,
+  addArticlePage,
+  articlePage,
+  addArticleFunction,
+  editArticlePage,
+  editArticleFunction,
+  deleteArticle,
+  admArticulosPAge,
 };
