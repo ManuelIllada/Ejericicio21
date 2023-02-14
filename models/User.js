95258cf13aa06032ac1ce538051f0ad0e05f9619
inavarroviana <inavarroviana@gmail.com>
2023-02-14 15:44:51 -0300

    password add

--- models/User.js
+++ models/User.js
@@ -1,31 +1,33 @@
 const { Model, DataTypes } = require("sequelize");
 
 class Users extends Model {
-    static initModel(sequelize) {
-
-        Users.init(
-            {
-                id: {
-                    primaryKey: true,
-                    autoIncrement: true,
-                    type: DataTypes.BIGINT,
-                },
-                nombre: {
-                    allowNull: false,
-                    type: DataTypes.STRING,
-                },
-                apellido: {
-                    allowNull: false,
-                    type: DataTypes.STRING,
-                },
-                email: {
-                    type: DataTypes.STRING,
-                },
-            },
-            { sequelize, modelName: "user", timestamps: false, createdAt: true }
-        );
-        return Users
-
-    }
+  static initModel(sequelize) {
+    Users.init(
+      {
+        id: {
+          primaryKey: true,
+          autoIncrement: true,
+          type: DataTypes.BIGINT,
+        },
+        nombre: {
+          allowNull: false,
+          type: DataTypes.STRING,
+        },
+        apellido: {
+          allowNull: false,
+          type: DataTypes.STRING,
+        },
+        email: {
+          type: DataTypes.STRING,
+        },
+        password: {
+          allowNull: false,
+          type: DataTypes.STRING,
+        },
+      },
+      { sequelize, modelName: "user", timestamps: false, createdAt: true }
+    );
+    return Users;
+  }
 }
-module.exports = Users
+module.exports = Users;
